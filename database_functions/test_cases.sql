1f8955c4d67f190ae64a629df201a2c66aa97431
alvoholic <96780096+alvoholic@users.noreply.github.com>
2025-10-24 07:05:33 +0700

    Create test_cases.sql

---
+++ database_functions/test_cases.sql
@@ -0,0 +1,7 @@
+-- test_cases.sql
+-- Create schema and sample data to test the functions
+
+
+-- Drop if exists (safe for repeated runs)
+DROP TABLE IF EXISTS audit_stock_changes CASCADE;
+DROP TABLE IF EXISTS stock_in
